ea3206525f08b42311a19661a1961a99e946720a
bai-3 <YF>
2026-03-01 16:09:25 +0800

    Refactor HexToH160 function to simplify prefix removal using strings.TrimPrefix for improved readability.

--- util/revive_func.go
+++ util/revive_func.go
@@ -48,9 +48,7 @@ func FuncToSelector(f string) [4]byte {
 // Convert a h160 hex string to a byte array
 func HexToH160(hexString string) (types.H160, error) {
 	// Remove the "0x" prefix if it exists
-	if strings.HasPrefix(hexString, "0x") {
-		hexString = hexString[2:]
-	}
+	hexString = strings.TrimPrefix(hexString, "0x")
 
 	// Decode the hexadecimal string to a byte slice
 	src := []byte(hexString)
